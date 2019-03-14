2d7d62c43e2761c775491cd7b083e8475b6777d5
mishkarch <miha239@mail.ru>
2019-03-14 18:52:17 +0300

    добавил управление мышью

--- Sprite.gd
+++ Sprite.gd
@@ -6,22 +6,28 @@ var vertvel = 0
 
 func _input(event):
 	if event.is_action_pressed("ui_wheel_up"):
-		vertvel = -1
+		vertvel -= 1
 		print('upp')
 	elif event.is_action_pressed("ui_wheel_down"):
-		vertvel = 1
+		vertvel += 1
 		print('dowwww')
 	else:
 		pass
-
+		#vertvel = 0
+	#get_parent().get_node("Label").text='vertvel = '+str(vertvel)
 
 
 func _process(delta):
+	get_parent().get_node("Label").text='vertvel '+str(vertvel)
 	#position = Vector2()
-	vertvel = 0
 	if Input.is_action_pressed("ui_right"):
 		position.x+=delta*SPEED
 	if Input.is_action_pressed("ui_left"):
 		position.x-=delta*SPEED
-	position.y += vertvel*delta*SPEED*10000
+	if Input.is_action_pressed("ui_mb4"):
+		position.y-=delta*SPEED
+	if Input.is_action_pressed("ui_mb5"):
+		position.y+=delta*SPEED
+
+	position.y += vertvel*SPEED*delta
 
